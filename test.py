2e89b65f76be7f8a45ffbe0fb8b830a3f3c0956b
Christian Garbin <garbin.christian@gmail.com>
2021-10-04 08:35:38 -0400

    Add test for the credit dataset

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 Create simple datasets to test the ridge regression code.
 """
 import copy
+import numpy as np
 
 import sklearn as sk
 from sklearn import linear_model
@@ -12,10 +13,13 @@ import utils
 import ridge
 
 
-def _test(data_file: str, lr: float, lmbda: float, iterations: int, max_mse: float, max_mse_diff: float) -> None:
+def _test(x: np.ndarray, y: np.ndarray, lr: float, lmbda: float, iterations: int, max_mse: float,
+          max_mse_diff: float) -> None:
     """Test the ridge regression code on a dataset, comparing with scikit-learn.
 
     Args:
+        x (np.ndarray): The input values, with categorical values (if present) encoded as binary values.
+        y (np.ndarray): The target values.
         data_file (str): The name of the dataset file.
         lr (float): The learning rate (alpha) to use for the gradient descent.
         lmbda (float): The regularization parameter (lambda) to use for the gradient descent.
@@ -24,9 +28,10 @@ def _test(data_file: str, lr: float, lmbda: float, iterations: int, max_mse: flo
         max_mse_diff (float): The maximum MSE difference allowed between our model and the model
             from scikit-learn before the test fails.
     """
-    print(f'\n\nTesting with {data_file}')
+    # Copy before we modify it
+    x_orig = copy.deepcopy(x)
+    y_orig = copy.deepcopy(y)
 
-    x, y = utils.read_dataset(data_file)
     utils.scale(x)
     utils.center(y)
 
@@ -44,8 +49,9 @@ def _test(data_file: str, lr: float, lmbda: float, iterations: int, max_mse: flo
     assert mse <= max_mse, f'MSE {mse} is too high, should be <= {max_mse}'
 
     # Now use scikit-learn on the same dataset
-    x_sk, y_sk = utils.read_dataset(data_file)
-    model = pipeline.make_pipeline(sk.preprocessing.StandardScaler(), linear_model.Ridge(alpha=0.1))
+    x_sk = copy.deepcopy(x_orig)
+    y_sk = copy.deepcopy(y_orig)
+    model = pipeline.make_pipeline(sk.preprocessing.StandardScaler(), linear_model.Ridge(alpha=lmbda))
     model.fit(x_sk, y_sk)
     predictions_sk = model.predict(x_sk)
     mse_sk = sk.metrics.mean_squared_error(y_sk, predictions_sk)
@@ -62,7 +68,9 @@ def _test(data_file: str, lr: float, lmbda: float, iterations: int, max_mse: flo
 
 def test_simple() -> None:
     """Test the code with a very simple model - it must perform well on it."""
-    # Create a dataset with a categorical feature
+    print('\n\nSimple dataset')
+
+    # Create a dataset with very simple features
     test_file_name = 'test_dataset_simple.csv'
     with open(test_file_name, 'w', encoding='utf-8') as test_file:
         test_file.write('a,b,a+b\n')
@@ -78,11 +86,14 @@ def test_simple() -> None:
     # close to what scikit-learn would do
     # We don't really need regularization for this case, but we use a small value to not hide a
     # possible error in the code if we simply set it to zero
-    _test(test_file_name, lr=0.0001, lmbda=0.001, iterations=100, max_mse=0.01, max_mse_diff=0.01)
+    x, y = utils.read_dataset(test_file_name)
+    _test(x, y, lr=0.0001, lmbda=0.001, iterations=100, max_mse=0.01, max_mse_diff=0.01)
 
 
 def test_categorical() -> None:
     """Test the code with a dataset that simulates a categorical variable."""
+    print('\n\nCategorical dataset')
+
     # Create a dataset with a categorical feature
     test_file_name = 'test_dataset_categorical.csv'
     with open(test_file_name, 'w', encoding='utf-8') as test_file:
@@ -95,10 +106,14 @@ def test_categorical() -> None:
 
     # We don't really need regularization for this case, but we use a small value to not hide a
     # possible error in the code if we simply set it to zero
-    _test(test_file_name, lr=0.0001, lmbda=0.1, iterations=1000, max_mse=250, max_mse_diff=0.1)
+    x, y = utils.read_dataset(test_file_name)
+    _test(x, y, lr=0.0001, lmbda=0.1, iterations=1000, max_mse=250, max_mse_diff=0.1)
 
 
-def credit():
+def test_credit():
+    """Test the code with the credit dataset."""
+    print('\n\nCredit dataset')
+
     file_name = './Credit_N400_p9.csv'
     x, y = utils.read_dataset(file_name)
 
@@ -107,42 +122,16 @@ def credit():
     utils.encode_binary_cateogry(x, column=7, one_value='Yes')  # student
     utils.encode_binary_cateogry(x, column=8, one_value='Yes')  # married
 
-    # Copy before we modify it
-    x_orig = copy.deepcopy(x)
-    y_orig = copy.deepcopy(y)
+    _test(x, y, lr=0.00001, lmbda=1_000, iterations=10_000, max_mse=100_000, max_mse_diff=1)
 
-    utils.scale(x)
-    utils.center(y)
 
-    coefficients = ridge.fit(x, y, lr=0.00005, lmbda=10000, iterations=10000)
-
-    predictions = utils.predict(x, coefficients)
-    mse = utils.mse(y, predictions)
-
-    print('\nsOur code')
-    print(f'MSE: {mse}')
-    print(f'Original input:\n{y[:3]}')
-    print(f'Predicted values:\n{predictions[:3]}')
-
-    # Now use scikit-learn on the same dataset
-    # Copy yet again because scikit-learn may modify it
-    x_sk = copy.deepcopy(x_orig)
-    y_sk = copy.deepcopy(y_orig)
-    model = pipeline.make_pipeline(sk.preprocessing.StandardScaler(), linear_model.Ridge(alpha=10000))
-    model.fit(x_sk, y_sk)
-    predictions_sk = model.predict(x_orig)
-    mse_sk = sk.metrics.mean_squared_error(y_sk, predictions_sk)
-
-    print('\nscikit-learn')
-    print(f'MSE: {mse_sk}')
-    print(f'Original input:\n{y_orig[:3]}')
-    print(f'Predicted values:\n{predictions_sk[:3]}')
+def test_all() -> None:
+    """Run all the tests."""
+    test_simple()
+    test_categorical()
+    test_credit()
 
 
 if __name__ == "__main__":
     utils.check_python_version()
-
-    test_simple()
-    test_categorical()
-
-    # credit()
+    test_all()
